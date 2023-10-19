0cb846cb90b25b26b7b58826be989788505a9abd
LiamJames04 <146339727+LiamJames04@users.noreply.github.com>
2023-10-19 14:55:02 +0100

    Update main.cpp

--- Tasks/Task-110-Variables/main.cpp
+++ Tasks/Task-110-Variables/main.cpp
@@ -21,14 +21,14 @@ BusOut ledData(LED_D0_PIN, LED_D1_PIN, LED_D2_PIN, LED_D3_PIN, LED_D4_PIN, LED_D
 
 int main()
 {
-    printf("\nTASK-110\n");
+    printf("\nTASK-110\n\n");
 
     // Integer
-    int count = 0;
+    int count = 10;
     printf("count = %d\n", count);
 
     // Add one to count
-    count = count + 1;
+    count = count + 2;
     printf("New value of count = %d\n", count);
 
     // Char
@@ -36,11 +36,30 @@ int main()
     character = 'A';
     printf("The character %c has the ASCII code %d\n", character, character);
 
+    char character1;
+    character1 = 'Z';
+    printf("The character %c has the ASCII code %d\n", character1, character1);
+
+    char character2;
+    character2 = 'a';
+    printf("The character %c has the ASCII code %d\n", character2, character2);
+
+    char character3;
+    character3 = 'z';
+    printf("The character %c has the ASCII code %d\n", character3, character3);
+
+    char character4;
+    character4 = '0';
+    printf("The character %c has the ASCII code %d\n", character4, character4);
+
+    char character5;
+    character5 = '9';
+    printf("The character %c has the ASCII code %d\n", character5, character5);
     // Short
-    short shortCount = 32766;
+    unsigned short shortCount = 32766;
     printf("The value of shortCount is %hd\n", shortCount);
-    shortCount = shortCount + 1;
-    printf("add 1 and shortCount is %hd\n", shortCount);
+    shortCount = shortCount + 2;
+    printf("add 2 and shortCount is %hd\n", shortCount);
 
     // Long long
     long long NN = 0x12345678ABCD0001LL; //Literal in HEX
@@ -48,18 +67,18 @@ int main()
     printf("A very large number in hex %llX\n", NN);
 
     // unsigned
-    unsigned short p = 1; //16 bit
-    printf("unsigned int p = %u\n", p);
+    short p = 1; //16 bit
+    printf("unsigned int p = %d\n", p);
     p = p - 2;
-    printf("Subtract 2. Now unsigned int p = %u\n", p);
+    printf("Subtract 2. Now unsigned int p = %d\n", p);
 
     // float 
     float pi_float = 3.1415926536;
-    printf("The value of pi is approximately %f\n", pi_float);
+    printf("The value of pi is approximately %.10f\n", pi_float);
 
     // double
-    double pi_double = 3.1415926536l;
-    printf("The value of pi is approximately %lf\n", pi_double);
+    double pi_double = 3.14159265358979323846l;
+    printf("The value of pi is approximately %.20lf\n", pi_double);
 
     // Data type sizes (in bytes)
     printf("Size of a char is %d bytes\n", sizeof(char));
