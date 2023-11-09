0a9f4d032e207e4adc895f60973427e92d377e41
LiamJames04 <146339727+LiamJames04@users.noreply.github.com>
2023-11-09 15:01:25 +0000

    Update main.cpp

--- Tasks/Task-130-if-elseif-else/main.cpp
+++ Tasks/Task-130-if-elseif-else/main.cpp
@@ -1,11 +1,11 @@
 #include "uop_msb.h"
 using namespace uop_msb;
 
-DigitalIn ButtonA(PG_0); //Button A
-DigitalIn ButtonB(PG_1); //Button B
+
 DigitalIn ButtonC(PG_2, PullDown); //Button C
 DigitalIn ButtonD(PG_3, PullDown); //Button D
-
+BusIn bus(PG_0, PG_1);
+int swVal;
 DigitalOut redLED(TRAF_RED1_PIN);       //Red Traffic 1
 DigitalOut yellowLED(TRAF_YEL1_PIN);    //Yellow Traffic 1
 DigitalOut greenLED(TRAF_GRN1_PIN);     //Green Traffic 1
@@ -26,30 +26,31 @@ int main()
     while (true) {
         
         //Read button without blocking
-        int btnA = ButtonA;     //Local to the while-loop  
-        int btnB = ButtonB;
+        swVal = bus;
         //Test Button A
-        if (btnA == 1){
+        if (bus[0] == 1){
             redLED = !redLED;    //Toggle RED led
-            count = count + 1;            //Increment count
+            count = count+ ((count<99) ? 1:0);            //Increment count
             disp = count;       //Update display
         }
-        else if (btnB==1){
-            yellowLED = !yellowLED;
-            count = count-1;
+        if (bus[1]==1){
+            redLED = !redLED;
+            count=count-1; 
             disp = count;
         }
-        else{
-            greenLED=!greenLED;
+        if((bus[0]==1) && (bus[1]==1)){
+            greenLED=1;
+            count=0;
+            disp=count;
         }
-
-        // Slow it down a bit (and debounce the switches)
-        wait_us(100000); 
-        if ((count=0) || (count=99)){
+        if((count<0) || (count>99)){
             count=0;
             disp=count;
         }
 
+        // Slow it down a bit (and debounce the switches)
+       wait_us(100000);
+       greenLED=0;
     }
 }
 
