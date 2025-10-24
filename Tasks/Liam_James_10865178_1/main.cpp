877a1d7c8b0366f9afb0e344ce1bc672d22f82b8
LiamJames04 <146339727+LiamJames04@users.noreply.github.com>
2025-10-24 15:53:14 +0100

    Update main.cpp

--- Tasks/Liam_James_10865178_1/main.cpp
+++ Tasks/Liam_James_10865178_1/main.cpp
@@ -4,19 +4,22 @@
 class Rectangle {
 
     private:
-    float length;
-    float width;
+    float length; // Defines legnth
+    float width;  // Defines Width
 
     public:
+    // This will initialize both the length and width
     Rectangle(float w, float l){
         length = l;
         width = w;
     }
 
+    //This is will calculate the area through this function
     float getarea() {
     return length * width;
     }
-
+    
+    //Calculates the perimeter through this function
     float getperimeter() {
 
     return 2 * (length + width);
@@ -26,10 +29,11 @@ class Rectangle {
 // main() runs in its own thread in the OS
 int main()
 {
-    printf("Program Starting................\n");
+    printf("Program Starting................\n"); // Stages the program is starting
 
-    Rectangle rect1(5.0, 3.0);
+    Rectangle rect1(5.0, 3.0); // Create a Rectangle through this function
 
+    //Outputs the area and perimeter of the rectangle
     printf("Rectangle Area: %.1f \n", rect1.getarea());
     printf("Rectangle Perimeter: %.1f \n", rect1.getperimeter());
 
